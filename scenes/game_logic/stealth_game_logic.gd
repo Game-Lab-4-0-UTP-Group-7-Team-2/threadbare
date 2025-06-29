01866aae1f17f7740f08cd01b97d9cba7c89b493
Fabiana <fabiana.castroc22@gmail.com>
2025-06-29 12:58:07 -0500

    mini juego 1 avance

--- scenes/game_logic/stealth_game_logic.gd
+++ scenes/game_logic/stealth_game_logic.gd
@@ -1,32 +1,30 @@
-# SPDX-FileCopyrightText: The Threadbare Authors
-# SPDX-License-Identifier: MPL-2.0
-@tool
-class_name StealthGameLogic
 extends Node
 
-@export_range(0.5, 3.0, 0.1, "or_greater", "or_less") var zoom: float = 1.0:
-	set = _set_zoom
+var orden_correcto: Array[String] = ["clip1", "clip2", "clip3", "clip4", "clip5"]
+
+func verificar_orden() -> void:
+	var linea_tiempo: HBoxContainer = get_node("../TileMapLayers/TimelineContainer")
+	var respuesta: Array[String] = []
+
+	for slot in linea_tiempo.get_children():
+		if slot.get_child_count() > 0:
+			var hijo: TextureButton = slot.get_child(0) as TextureButton
+			respuesta.append(hijo.name)
+		else:
+			respuesta.append("vacio")
+
+	if respuesta == orden_correcto:
+		mostrar_resultado("✅ ¡Memoria reconstruida correctamente!", Color(0, 1, 0))
+	else:
+		mostrar_resultado("❌ Memoria distorsionada...", Color(1, 0, 0))
 
 
-func _ready() -> void:
-	if Engine.is_editor_hint():
-		return
-	_set_zoom(zoom)
-	for guard: Guard in get_tree().get_nodes_in_group(&"guard_enemy"):
-		guard.player_detected.connect(self._on_player_detected)
+func mostrar_resultado(texto: String, color: Color) -> void:
+	if has_node("../TileMapLayers/ResultadoLabel"):
+		var label: Label = get_node("../TileMapLayers/ResultadoLabel") as Label
+		label.text = texto
+		label.modulate = color
 
 
-func _on_player_detected(player: Node2D) -> void:
-	player.mode = Player.Mode.DEFEATED
-	await get_tree().create_timer(2.0).timeout
-	SceneSwitcher.reload_with_transition(Transition.Effect.FADE, Transition.Effect.FADE)
-
-
-func _set_zoom(new_value: float) -> void:
-	zoom = new_value
-	if Engine.is_editor_hint():
-		return
-	if not is_node_ready():
-		return
-	var camera: Camera2D = get_viewport().get_camera_2d()
-	camera.zoom = Vector2.ONE * zoom
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
